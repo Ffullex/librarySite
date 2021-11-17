c06097fc7b3c449e77a5a578ca5f23fc2e8e4aff
ffullex <mr.tomanovskiy@mail.ru>
2021-11-17 12:06:46 +0700

    Добавил книги в коллекцию книг

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -9,9 +9,36 @@ namespace WebAppWithBootsTRAP.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly BooksCatalog _booksCatalog;
+        
+        public BooksController()
+        {
+            _booksCatalog = new BooksCatalog();
+
+            _booksCatalog.AddBook(new Book
+            {
+                Title = "Headless",
+                Author = "Head User",
+                Genre = "reality",
+                InventoryNumber = Guid.NewGuid().ToString(),
+                ISBN = "023874074932",
+                YearOfEdition = "1488",
+            });
+
+            _booksCatalog.AddBook(new Book
+            {
+                Title = "Headless",
+                Author = "Head User",
+                Genre = "reality",
+                InventoryNumber = Guid.NewGuid().ToString(),
+                ISBN = "023874074932",
+                YearOfEdition = "1488",
+            });
+        }
         public IActionResult Index()
         {
-            return View();
+            var books = _booksCatalog.GetAll();
+            return View(books);
         }
     }
 }
